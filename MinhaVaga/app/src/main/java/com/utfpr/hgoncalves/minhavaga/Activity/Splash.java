97bd31b4215e4e5e8d9d446fb0cb5d23dce65f44
Humberto <humberto.g.moreira@gmail.com>
2015-11-27 01:58:23 -0200

    Login funciona

--- MinhaVaga/app/src/main/java/com/utfpr/hgoncalves/minhavaga/Activity/Splash.java
+++ MinhaVaga/app/src/main/java/com/utfpr/hgoncalves/minhavaga/Activity/Splash.java
@@ -3,71 +3,90 @@ package com.utfpr.hgoncalves.minhavaga.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
 import android.widget.TextView;
 
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
 import com.facebook.LoggingBehavior;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 import com.utfpr.hgoncalves.minhavaga.Model.Usuario;
 import com.utfpr.hgoncalves.minhavaga.R;
+
+import org.json.JSONObject;
+
+import java.util.Arrays;
 //com.facebook.FacebookSdk
 
 public class Splash extends AppCompatActivity {
+    private static final String TAG = "NOVA";
     private TextView info;
     private LoginButton loginButton;
     private Usuario user;
-    //
-//    private Facebook facebook;
-//    private AsyncFacebookRunner mAsyncRunner;
     private CallbackManager callbackManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        final Intent intent = new Intent(this, Main2Activity.class);
+
         FacebookSdk.sdkInitialize(getApplicationContext());
         if (com.facebook.BuildConfig.DEBUG) {
             FacebookSdk.setIsDebugEnabled(true);
             FacebookSdk.addLoggingBehavior(LoggingBehavior.INCLUDE_ACCESS_TOKENS);
         }
-        callbackManager = CallbackManager.Factory.create();
         setContentView(R.layout.activity_splash);
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
-        final Intent intent = new Intent(this, Main2Activity.class);
-        info = (TextView) findViewById(R.id.info);
-        loginButton = (LoginButton) findViewById(R.id.login_button);
+    //pega status de logado acesstoken!=null logado
+        AccessToken accessToken = AccessToken.getCurrentAccessToken();
+        if (accessToken != null) {
 
+            startActivity(intent);
+        } else {
+            callbackManager = CallbackManager.Factory.create();
+            info = (TextView) findViewById(R.id.info);
+            loginButton = (LoginButton) findViewById(R.id.login_button);
+            loginButton.setReadPermissions(Arrays.asList("public_profile, email, user_birthday, user_friends"));
+            loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+                @Override
+                public void onSuccess(LoginResult loginResult) {
 
-//        facebook = new Facebook(APP_ID);
-//            mAsyncRunner = new AsyncFacebookRunner(facebook);
+                    // App code
+                    GraphRequest request = GraphRequest.newMeRequest(
+                            loginResult.getAccessToken(),
+                            new GraphRequest.GraphJSONObjectCallback() {
+                                @Override
+                                public void onCompleted(
+                                        JSONObject object,
+                                        GraphResponse response) {
+                                    // Application code
+                                    Log.v("LoginActivity", response.toString());
+                                }
+                            });
+                    startActivity(intent);
 
+                }
 
-        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
-            @Override
-            public void onSuccess(LoginResult loginResult) {
+                @Override
+                public void onCancel() {
+                    // App code
+                    Log.v("LoginActivity", "cancel");
+                }
 
-//                Usuario user = new Usuario();
-//                Gson gson = new GsonBuilder().create();
-//                gson.getAdapter(Usuario.class);
-                info.setText("DEU CERTO ");
-//                AppEventsLogger.activateApp(getApplication());
-            }
-
-
-            @Override
-            public void onCancel() {
-//                AppEventsLogger.deactivateApp(getApplication());
-            }
-
-            @Override
-            public void onError(FacebookException e) {
-                info.setText("Login attempt failed.");
-            }
-        });
+                @Override
+                public void onError(FacebookException exception) {
+                    // App code
+                    Log.v("LoginActivity", exception.getCause().toString());
+                }
+            });
+        }
     }
 
     @Override
@@ -76,5 +95,11 @@ public class Splash extends AppCompatActivity {
     }
 
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
 }
 
