e0753df4d208fb32929df6a0a3c2e826af80b587
Humberto <humberto.g.moreira@gmail.com>
2015-12-02 20:12:08 -0200

    Dando cara ao app

--- MinhaVaga/app/src/main/java/com/utfpr/hgoncalves/minhavaga/Activity/Splash.java
+++ MinhaVaga/app/src/main/java/com/utfpr/hgoncalves/minhavaga/Activity/Splash.java
@@ -1,9 +1,12 @@
 package com.utfpr.hgoncalves.minhavaga.Activity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 import com.facebook.AccessToken;
@@ -33,27 +36,31 @@ public class Splash extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         final Intent intent = new Intent(this, Main2Activity.class);
 
+
         FacebookSdk.sdkInitialize(getApplicationContext());
         if (com.facebook.BuildConfig.DEBUG) {
             FacebookSdk.setIsDebugEnabled(true);
             FacebookSdk.addLoggingBehavior(LoggingBehavior.INCLUDE_ACCESS_TOKENS);
         }
+
         setContentView(R.layout.activity_splash);
-    //pega status de logado acesstoken!=null logado
         AccessToken accessToken = AccessToken.getCurrentAccessToken();
-        if (accessToken != null) {
-            startActivity(intent);
-        } else {
-            callbackManager = CallbackManager.Factory.create();
-            info = (TextView) findViewById(R.id.info);
-            loginButton = (LoginButton) findViewById(R.id.login_button);
-            loginButton.setReadPermissions(Arrays.asList("public_profile, email, user_birthday, user_friends"));
-            loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
-                @Override
-                public void onSuccess(LoginResult loginResult) {
+
+        if (verificaConexao()) {
+
+            if (accessToken != null) {
+                startActivity(intent);
+            } else {
+
+                callbackManager = CallbackManager.Factory.create();
+                info = (TextView) findViewById(R.id.info);
+                loginButton.setReadPermissions(Arrays.asList("public_profile, email, user_birthday, user_friends"));
+                loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
 
 
-                    // App code
+                        // App code
 //                    GraphRequest request = GraphRequest.newMeRequest(
 //                            loginResult.getAccessToken(),
 //                            new GraphRequest.GraphJSONObjectCallback() {
@@ -65,29 +72,55 @@ public class Splash extends AppCompatActivity {
 //                                    Log.v("LoginActivity", response.toString());
 //                                }
 //                            });
-                    startActivity(intent);
+                        startActivity(intent);
 
-                }
+                    }
 
-                @Override
-                public void onCancel() {
-                    // App code
-                    Log.v("LoginActivity", "cancel");
-                }
+                    @Override
+                    public void onCancel() {
+                        // App code
+                        Log.v("LoginActivity", "cancel");
+                    }
 
-                @Override
-                public void onError(FacebookException exception) {
-                    // App code
-                    Log.v("LoginActivity", exception.getCause().toString());
-                }
-            });
+                    @Override
+                    public void onError(FacebookException exception) {
+                        // App code
+                        Log.v("LoginActivity", exception.getCause().toString());
+                    }
+                });
+
+            }
+        } else {
+            loginButton = (LoginButton) findViewById(R.id.login_button);
+            loginButton.setVisibility(View.VISIBLE);
+
+            info = (TextView) findViewById(R.id.info);
+//            info.setEnabled(false);
+            info.setVisibility(View.VISIBLE);
+            info.setText("Sem conexão com a internet");
+//            info.setVisibility();
+
+            //Toast.makeText(this, "SEM CONEXÂO COM A INTERNET", Toast.LENGTH_LONG).show();
+
         }
     }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
+
+    public boolean verificaConexao() {
+        ConnectivityManager conectivtyManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (conectivtyManager.getActiveNetworkInfo() != null
+                && conectivtyManager.getActiveNetworkInfo().isAvailable()
+                && conectivtyManager.getActiveNetworkInfo().isConnected()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
 
